2040003cee076295d94accfb7ffdcc3fd49ee632
Gabriela Perez <gabrielaperez1127@gmail.com>
2017-03-25 16:40:30 -0500

    librerias

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'); 
+
+
+gulp.task('portafolio-win', function(){
+    gulp.src('js/source/*js').pipe(concat('todo.js')).pipe(uglify()).pipe(gulp.dest('js/build/'))
+});
