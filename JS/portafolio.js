18f8a26412ebc98af445abed8871a041a22dcf4b
Gabriela Perez <gabrielaperez1127@gmail.com>
2017-03-26 16:19:07 -0500

    maquetación

---
+++ JS/portafolio.js
@@ -0,0 +1,67 @@
+
+ /* function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+    
+}
+
+let progress = 0;
+const fakeLoaderInterval = window.setInterval(function() {
+  const $lp = $('.loading-progress');
+  progress = progress + getRandomArbitrary(10, 25)
+  $lp.css('transform', `translateX(${progress}%)`);
+  if (progress >= 75) {
+    window.clearInterval(fakeLoaderInterval);
+    $lp.css('transform', 'translateX(100%)');
+    setTimeout(() => $('.loading').css('transform', 'translateY(calc(100% + 10px))'), 400);
+  }
+}, getRandomArbitrary(100, 500));*/
+
+(function() {
+  $('#fullpage').fullpage({
+      anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage'],
+      menu: '#menu',
+      css3: true
+  });
+   
+})();
+
+var lista = $('#list-menu');
+lista.children().click(clase);
+
+function clase(){
+	lista.children().removeClass('tab-current');
+	$(this).addClass('tab-current'); 
+	
+}
+
+var current = 1; 
+var height = $('.roles').height();
+var numberDivs = $('.roles').children().length; 
+var first = $('.roles div:nth-child(1)'); 
+setInterval(function() {
+    var number = current * -height;
+    first.css('margin-top', number + 'px');
+    if (current === numberDivs) {
+        first.css('margin-top', '0px');
+        current = 1;
+    } else current++;
+}, 2000);
+
+
+$('.spotlight__item').click(function() {
+  
+  $(this).toggleClass('item--active');
+  
+})
+
+//current position
+var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
+
+
+	
+
+
